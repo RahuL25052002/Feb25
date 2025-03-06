4d0d261728a72a85d5be957863a8398f1ad9508e
Rahul25052002 <rnikale55@gmail.com>
2025-03-07 00:29:01 +0530

    Day-4 code

---
+++ OOPJ_module-2/Day-4/Encapsulation.java
@@ -0,0 +1,21 @@
+class Account{
+
+	private double balance;
+	public double getBalance(){
+
+		return balance;
+	}
+
+	public void setBalance(double amount){
+
+		this.balance=this.balance+amount;
+	}
+}
+class user{
+
+	public static void main(String[]args){
+		
+
+	}
+
+}
