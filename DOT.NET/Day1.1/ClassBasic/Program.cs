21069f7f1d5134cab38d2fe300016d5a5b399787
Rahul25052002 <rnikale55@gmail.com>
2025-06-16 00:37:12 +0530

    DOT NET ADDED

---
+++ DOT.NET/Day1.1/ClassBasic/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Class1 obj=new Class1();
+            obj.Display();
+        }
+    }
+    public class Class1
+    {
+
+        public void Display()
+        {
+            System.Console.WriteLine("Display called");
+        }
+    }
+
+}
---
+++ DOT.NET/Day1/Employee/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EmployeeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public  class Employee
+    {
+        String Name;
+        int EmpNo;
+        decimal Basic;
+        private short deptNo;
+
+        public int DeptNo
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    DeptNo = value;
+                }
+            }
+            get
+            {
+                return DeptNo;
+            }
+        }
+        decimal GetNetSalary()
+        {
+            return 0;
+        }
+    }
+}
