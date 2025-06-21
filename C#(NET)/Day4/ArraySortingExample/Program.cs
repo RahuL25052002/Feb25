aa2505c8396ebe7e7bcf44487e5310d2e7fcc35b
Rahul25052002 <rnikale55@gmail.com>
2025-06-21 15:49:01 +0530

    Day-5 code added

--- C#(NET)/Day4/ArraySortingExample/Program.cs
+++ C#(NET)/Day4/ArraySortingExample/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Employee1[] arr = new Employee1[5];
             arr[0] = new Employee1 { EmpNo = 5, Name = "Junaid", Basic = 12335, DeptNo = 10 };
@@ -22,7 +22,29 @@
                 Console.WriteLine(item);
             }
         }
+
+        static void Main()
+        {
+            Employee2[] arr = new Employee2[5];
+            arr[0] = new Employee2 { EmpNo = 5, Name = "Junaid", Basic = 12335, DeptNo = 10 };
+            arr[1] = new Employee2 { EmpNo = 2, Name = "Abhi", Basic = 22335, DeptNo = 20 };
+            arr[2] = new Employee2 { EmpNo = 1, Name = "Avni", Basic = 52335, DeptNo = 20 };
+            arr[3] = new Employee2 { EmpNo = 4, Name = "Shiti", Basic = 32335, DeptNo = 30 };
+            arr[4] = new Employee2 { EmpNo = 3, Name = "Suruchi", Basic = 42335, DeptNo = 40 };
+
+            //Array.Sort<Employee1>(arr );
+
+            // Array.Sort(arr, new Employee1()); //IComparer
+            Array.Sort<Employee2>(arr); //IComparer
+
+
+            foreach (Employee2 item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
+
 
     public class Class1 : IComparer<Employee1>
     {
@@ -41,6 +63,25 @@
         public string Name { get; set; }
         public decimal Basic { get; set; }
         public short DeptNo { get; set; }
+        public override string ToString()
+        {
+            return $"Empno={EmpNo}, Name={Name}, Basic={Basic}, DeptNo={DeptNo}";
+        }
+    }
+    public class Employee2:IComparable<Employee2>
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public short DeptNo { get; set; }
+
+        public int CompareTo(Employee2? other)
+        {
+            if(this.DeptNo>other.DeptNo) return -1;
+            else if(this.EmpNo>other.EmpNo) return 1;
+            return 0;
+        }
+
         public override string ToString()
         {
             return $"Empno={EmpNo}, Name={Name}, Basic={Basic}, DeptNo={DeptNo}";
---
+++ C#(NET)/Day4/Params/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(Add(1, 2, 3, 4, 5, 5));
+            //int[]arr = { 1, 2,3,4, 5, 6, 7 };
+            //Console.WriteLine(Add(1, arr[]));
+        }
+        //public static int Add(int x,params int[] values) {
+        public static int Add(params int[] values)
+        {
+            int add = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                add += values[i];
+            }
+            return add;
+        }
+    }
+}
